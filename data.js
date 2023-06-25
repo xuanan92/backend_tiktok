e3d54065e48870ceb85ac1378f34372c1f7033cd
root <root@XuanAnPC>
2023-06-25 22:22:50 +0700

    update heroku

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    name: "xuanan92",
+    password: "hlelo"
+  },
+  {
+    name: "hlgy9292",
+    password: "hlelo"
+  }
+]
+
+export default data
---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Extrain from './data.js'
+
+// app config
+const app = express()
+const port = 9000
+
+// DB config
+const connection_url = "mongodb+srv://nguyentruongxuananhn:superAnIT01!@cluster0.ytehc68.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+// api endpoint
+app.get('/', (req, res) => res.status(200).send("hllo there"))
+app.get('/posts', (req, res) => res.status(200).send(Extrain))
+
+app.post('/posts', (req, res) => {
+  const dbName = req.body
+  Data.create(dbName, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+})
+
+
+// app listener
+app.listen(port, () => console.log(`Listen on port: ${port}`))
