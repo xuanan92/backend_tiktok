c0e410961ed8af52f1907e33e35f85f1ea9186ee
root <root@XuanAnPC>
2023-07-09 00:45:33 +0700

    first init

--- dbModel.js
+++ dbModel.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose"
 
 const tiktokSchema = mongoose.Schema({
-  name: String,
-  password: String,
+  channel: String,
+  ticker: Boolean,
+  channel_name: String,
+  channel_image: String,
+  status: String,
+  music: String,
+  music_url: String,
+  url_Video: String,
+  heart: String,
+  comment: String,
+  mark: String,
+  share: String
 })
 
 export default mongoose.model('tiktokVideos', tiktokSchema)
